5f6a5fdbdb41c41c826ec3535c0057140717835b
binamkayastha <binamkayastha@gmail.com>
2015-09-10 13:34:50 -0400

    Changed to Linux, so all the line endings were changed to have added '^M'

--- js/js.js
+++ js/js.js
@@ -1,6 +1,37 @@
-window.onload = function(e) {
-	$("#MainDiv").load("currentEvents.html");
+// window.onload = function(e) {
+	// $("#MainDiv").load("currentEvents.html");
+
+// }
+
+window.onload = function () {
+		$("#MainDiv").load("currentEvents.html");
+
+    if (typeof history.pushState === "function") {
+        history.pushState("jibberish", null, null);
+        window.onpopstate = function () {
+            history.pushState('newjibberish', null, null);
+            // Handle the back (or forward) buttons here
+            // Will NOT handle refresh, use onbeforeunload for this.
+        };
+    }
+    else {
+        var ignoreHashChange = true;
+        window.onhashchange = function () {
+            if (!ignoreHashChange) {
+                ignoreHashChange = true;
+                window.location.hash = Math.random();
+                // Detect and redirect change here
+                // Works in older FF and IE9
+                // * it does mess with your hash symbol (anchor?) pound sign
+                // delimiter on the end of the URL
+            }
+            else {
+                ignoreHashChange = false;
+            }
+        };
+    }
 }
+
 function changePage(page) {
-    $("#MainDiv").load(page);
+    $("#MainDiv").load(page).hide().fadeIn("slow");
 }
